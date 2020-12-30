781963cc6814d3473d4d8a0da32190f03b648de1
SPrio <mahantasupriyo@gmail.com>
2020-12-30 20:57:24 +0530

    Added Name Text Below Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,22 @@ class MyApp extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('S4S Test'),
-      )
+      ),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.only(top: 40),
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Supriyo Mahanta',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
