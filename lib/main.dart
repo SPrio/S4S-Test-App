9e1b1cc6737562cc965d3539c321cf64d8390ff8
SPrio <mahantasupriyo@gmail.com>
2020-12-30 21:50:10 +0530

    added location coordinates text

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
-void main() {
-  runApp(
-    MaterialApp(home: MyApp()),
-  );
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Position _currentPosition;
+
+  @override
+  initState() {
+    super.initState();
+
+    final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+
+    geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+      });
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +58,8 @@ class MyApp extends StatelessWidget {
                   fontSize: 20,
                 ),
               ),
+              Text(
+                  "Location Co-ordinates: ${_currentPosition.latitude}, ${_currentPosition.longitude}"),
             ],
           ),
         ),
